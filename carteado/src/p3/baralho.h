9237e5f98587aef343505c1878211971d4a3894c
adolfostsi <adolfostsi@hotmail.com>
2017-05-16 17:16:39 -0300

    Update baralho.h

--- carteado/src/p3/baralho.h
+++ carteado/src/p3/baralho.h
@@ -8,13 +8,14 @@
 #ifndef BARALHO_H_
 #define BARALHO_H_
 
+#include <iostream>
 #include <vector>
 #include <random>
 #include <algorithm>
 #include <iterator>
 
-
 #include "carta.h"
+#include "jogador.h"
 
 namespace p3 {
 
@@ -22,14 +23,10 @@ class Baralho {
 public:
 	Baralho(int n_numerocartas);
 	virtual ~Baralho();
-
 	void embaralhar();
-
 	Carta pega_topo(void);
 	Carta pega_baixo(void);
-
-	void distribuir(int qtd_cartas);
-
+	void distribuir(unsigned int qtd_cartas,std::vector<Jogador> jogadores, bool cima);
 private:
 	int _numerocartas;
 	std::vector<Carta> _monte;
