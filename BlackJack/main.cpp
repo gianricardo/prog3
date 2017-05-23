7256f93cd3232606d1ce2fb4382dc17d5520b8b8
Andre Luigi <andreluigi@144radio215.wireless.ufsc.br>
2017-05-23 14:41:36 -0300

    oi

---
+++ BlackJack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  21
+//
+//  Created by Andre Luigi on 22/05/17.
+//  Copyright © 2017 Andre Luigi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(void) {
+    // insert code here...
+    
+    return 0;
+}
