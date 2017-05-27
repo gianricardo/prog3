afc3fa78e11d398d1be840bd1abc5c08632dccf5
Samuel Possamai <possamai.sam@gmail.com>
2017-05-27 14:22:25 -0300

    Generalizacao de jogador

--- carteado/src/p3/jogador.cpp
+++ carteado/src/p3/jogador.cpp
@@ -9,67 +9,6 @@
 
 namespace p3 {
 
-/* Recebe o nome no construtor */
-Jogador::Jogador(std::string name/* = "" */) : _nome{name}, _pontuacao{0}, _apto_para_jogar{true}{
-}
-
-Jogador::~Jogador() {
-
-}
-
-std::string Jogador::nome() const {
-
-	return _nome;
-}
-
-/* Recebe uma carta como argumento e insere no final
- * do vector _mao. */
-void Jogador::recebe_carta(Carta card){
-	_mao.push_back(card);
-}
-
-bool Jogador::tira_carta(Carta card, bool cmp_face /* = false */){
-
-	for(auto it = _mao.begin(); it != _mao.end(); it++){
-
-		if(*it == card){
-
-			if(cmp_face) if(it->mostra() != card.mostra()) continue;
-
-			_mao.erase(it);
-
-			return true;
-		}
-	}
-	
-	return false;
-}
-
-int Jogador::pontuacao() const {
-
-	return _pontuacao;
-}
-
-void Jogador::pontuacao(int p){
-
-	_pontuacao = p;
-}
-
-void Jogador::muda_aptidao(){
-	 _apto_para_jogar = !( _apto_para_jogar);
-}
-
-bool Jogador::esta_apto() const {
-	
-	return _apto_para_jogar;
-}
-
-
-const std::vector<Carta> Jogador::mostra_mao() const {
-
-	return _mao;
-}
-
 
 } /* namespace p3 */
 
--- carteado/src/p3/jogador.h
+++ carteado/src/p3/jogador.h
@@ -13,21 +13,22 @@
 
 namespace p3 {
 
-class Jogador {
+template<class CARTA>
+class JogadorBasico {
 public:
 
     //Cria um jogador com o nome "name"
-	Jogador(std::string name = "");
-	virtual ~Jogador();
+	JogadorBasico(std::string name = "");
+	virtual ~JogadorBasico();
 
     //Retorna o nome do jogador
 	std::string nome() const;
 
     //Acrescenta uma carta a mao do jogador
-	void recebe_carta(Carta card);
+	void recebe_carta(CARTA card);
 
     //Retira uma carta da mao do jogador, retorna false caso a carta nao esteja na mao
-	bool tira_carta(Carta card, bool cmp_face = false);
+	bool tira_carta(CARTA card, bool cmp_face = false);
 
     //Retorna a pontuacao atual do jogador
 	int pontuacao() const;
@@ -42,14 +43,77 @@ public:
 	bool esta_apto() const;
 
     //Retorna um vetor das cartas contidas na mao do jogador
-	const std::vector<Carta> mostra_mao() const ;
+	const std::vector<CARTA> mostra_mao() const ;
 
 private:
-	std::vector<Carta> _mao;
+	std::vector<CARTA> _mao;
 	std::string _nome;
 	int _pontuacao;
 	bool _apto_para_jogar;
 };
+
+/* Recebe o nome no construtor */
+template<class CARTA> JogadorBasico<CARTA>::JogadorBasico(std::string name/* = "" */) : _nome{name}, _pontuacao{0}, _apto_para_jogar{true}{
+}
+
+template<class CARTA> JogadorBasico<CARTA>::~JogadorBasico() {
+
+}
+
+template<class CARTA> std::string JogadorBasico<CARTA>::nome() const {
+
+	return _nome;
+}
+
+/* Recebe uma carta como argumento e insere no final
+ * do vector _mao. */
+template<class CARTA> void JogadorBasico<CARTA>::recebe_carta(CARTA card){
+	_mao.push_back(card);
+}
+
+template<class CARTA> bool JogadorBasico<CARTA>::tira_carta(CARTA card, bool cmp_face /* = false */){
+
+	for(auto it = _mao.begin(); it != _mao.end(); it++){
+
+		if(*it == card){
+
+			if(cmp_face) if(it->mostra() != card.mostra()) continue;
+
+			_mao.erase(it);
+
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+template<class CARTA> int JogadorBasico<CARTA>::pontuacao() const {
+
+	return _pontuacao;
+}
+
+template<class CARTA> void JogadorBasico<CARTA>::pontuacao(int p){
+
+	_pontuacao = p;
+}
+
+template<class CARTA> void JogadorBasico<CARTA>::muda_aptidao(){
+	 _apto_para_jogar = !( _apto_para_jogar);
+}
+
+template<class CARTA> bool JogadorBasico<CARTA>::esta_apto() const {
+	
+	return _apto_para_jogar;
+}
+
+
+template<class CARTA> const std::vector<CARTA> JogadorBasico<CARTA>::mostra_mao() const {
+
+	return _mao;
+}
+
+using Jogador = JogadorBasico<Carta>;
 
 } /* namespace p3 */
 
