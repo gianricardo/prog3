99606ee0484e71609eb76ac300d2c78b039963fa
Andre Luigi <andreluigi@145client54.wireless.ufsc.br>
2017-06-07 09:50:44 -0300

    m

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -40,24 +40,26 @@ int main(void) {
     int $p = 10000;
     // int gamble =0;
     
-    Regra Rules(2,2,-1,-1,0,52, (Regra::modo_fim::pontuacao), (Regra::condicao_vitoria::padrao), (Regra::condicao_derrota::zero_pontos));
-    JogoBasico<Carta> Game(&Rules, {"Dealer","Player"});
+    Regra *Rules = new Regra(2,2,-1,-1,0,52, (Regra::modo_fim::pontuacao), (Regra::condicao_vitoria::padrao), (Regra::condicao_derrota::zero_pontos));
+    JogoBasico<Carta> Game(Rules, {"Dealer","Player"});
     
     Game.novo_monte(); //cria monte do dealder
     Game.novo_monte(); //cria monte do jogador
-    Game.novo_monte(); //cria monte de split do gogador;
     Game.vira_carta_jogador(0); //coloca uma carta do dealer virada/
     hand=Game.mostra_mao_jogador_atual(); //pega ref. mao do dealer
-    Game.move_carta_jm(hand[0],0,0,true); //coloca as cartas do dealer no seu monte
-    Game.move_carta_jm(hand[1],0,0,true);
+    Game.move_carta_jm(hand[0],0,1,true); //coloca as cartas do dealer no seu monte
+    Game.move_carta_jm(hand[1],0,1,true);
     Game.jogador_soma_pontos($h); //coloca o dinheiro do dealer
     Game.fim_jogada(); //passa pro player
     hand=Game.mostra_mao_jogador_atual(); //pega ref. mao jogador
-    Game.move_carta_jm(hand[0],1,1,true); //passa as cartas pro monte do jogador
-    Game.move_carta_jm(hand[1],1,1,true);
+    Game.move_carta_jm(hand[0],1,2,true); //passa as cartas pro monte do jogador
+    Game.move_carta_jm(hand[1],1,2,true);
     Game.jogador_soma_pontos($p);//coloca o dinheiro do jogador;
     
-    //mostrar os montes
+    std::vector<std::pair<bool,Carta>> monte;
+    monte = Game.mostra_monte(0);
+    
+    std::cout<<monte[1].second.numero()<<std::endl;
     
     
     
