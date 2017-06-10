e7b4e060c47f0f368a87c5c1486f3834f1debd31
Vitor <vitor.russi@hotmail.com>
2017-06-10 14:58:13 -0300

    Teste do push agora vai

---
+++ Napoleao/jogador_napoleao.h
@@ -0,0 +1,17 @@
+/*
+ * jogador_napoleao.h
+ *
+ *  Created on: 9 de jun de 2017
+ *      Author: vitorrussi
+ */
+
+#ifndef JOGADOR_NAPOLEAO_H_
+#define JOGADOR_NAPOLEAO_H_
+
+class Jogador_napoleao {
+public:
+	Jogador_napoleao();
+	virtual ~Jogador_napoleao();
+};
+
+#endif /* JOGADOR_NAPOLEAO_H_ */
