54da7ba433b2f10b1efeb5aca7be65bc63459f70
Samuel Possamai <possamai.sam@gmail.com>
2017-06-26 15:07:06 -0300

    PacienciaSpyder: atualizacao

--- PacienciaSpyder/rules.cpp
+++ PacienciaSpyder/rules.cpp
@@ -1,7 +1,7 @@
 
 #include "rules.h"
 
-Rules::Rules() : p3::Regra(1, 0, 6, -1, -1, 114, Regra::modo_fim::rodadas,
+Rules::Rules() : p3::Regra(1, 0, 6, -1, -1, 104, Regra::modo_fim::rodadas,
                            Regra::condicao_vitoria::padrao,
                            Regra::condicao_derrota::nenhuma, 10) {
     
